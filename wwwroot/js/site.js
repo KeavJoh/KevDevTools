617829eb695776833fa7e2ccbeb752c7f1af900f
Kevin Johr <kevin.johr@posteo.de>
2024-06-26 16:11:21 +0200

    #2 add first idea of new navigation

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,30 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+const sidenav = document.getElementById('full-screen-example');
+const sidenavInstance = Sidenav.getInstance(sidenav);
+
+let innerWidth = null;
+
+const setMode = (e) => {
+    // Check necessary for Android devices
+    if (window.innerWidth === innerWidth) {
+        return;
+    }
+
+    innerWidth = window.innerWidth;
+
+    if (window.innerWidth < 660) {
+        sidenavInstance.changeMode('over');
+        sidenavInstance.hide();
+    } else {
+        sidenavInstance.changeMode('side');
+        sidenavInstance.show();
+    }
+};
+
+setMode();
+
+// Event listeners
+window.addEventListener('resize', setMode);
