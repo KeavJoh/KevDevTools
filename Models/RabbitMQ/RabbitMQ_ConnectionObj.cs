e4cb5e9c92666f663a544a081cd471fdbcccc070
Kevin Johr <kevin.johr@posteo.de>
2024-06-27 09:33:21 +0200

    #3 add rabbit model

---
+++ Models/RabbitMQ/RabbitMQ_ConnectionObj.cs
@@ -0,0 +1,16 @@
+﻿namespace KevDevTools.Models.RabbitMQ
+{
+    public class RabbitMQ_ConnectionObj
+    {
+        public string HostName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string VirtualHost { get; set; }
+        public int Port { get; set; }
+        public string QueueName { get; set; }
+        public bool Durable { get; set; }
+        public bool Exclusive { get; set; }
+        public bool AutoDelete { get; set; }
+        public bool Arguments { get; set; }
+    }
+}
